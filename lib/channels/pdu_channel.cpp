28eeff36276a0c55bd981cca2ae82b3b60e3f0f1
Ethan Chee <ethancheez@gmail.com>
2022-11-01 19:58:12 -1000

    Add sleep to rfm23 library, add loop to pdu channel, update main

--- lib/channels/pdu_channel.cpp
+++ lib/channels/pdu_channel.cpp
@@ -1,12 +1,19 @@
 #include <artemis_channels.h>
 
+Artemis::Teensy::PDU pdu(115200);
+PacketComm packet;
+
 void Artemis::Teensy::Channels::pdu_channel()
 {
-    Artemis::Teensy::PDU pdu(115200);
-
     // Enable burn wire
     pdu.PDU_SWITCH(Artemis::Teensy::PDU::BURN, true);
     delay(30000);
     pdu.PDU_SWITCH(Artemis::Teensy::PDU::BURN, false);
 
+    while (true)
+    {
+        if (PullQueue(&packet, pdu_queue, pdu_queue_mtx))
+        {
+        }
+    }
 }
