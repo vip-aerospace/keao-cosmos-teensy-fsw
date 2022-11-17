2a0bd333adc8b0087be8cb5a0dcafa0522ba8c31
Ethan Chee <ethancheez@gmail.com>
2022-11-16 18:00:27 -1000

    RFM23 send now takes in a packet

--- lib/rfm23/rfm23.cpp
+++ lib/rfm23/rfm23.cpp
@@ -30,12 +30,12 @@ namespace Artemis
                 rfm23.setFrequency(RFM23_FREQ);   // frequency default is 434MHz
                 rfm23.setTxPower(RFM23_TX_POWER); // 20 is the max
 
-                /* RFM23 modulation schemes and data rates
-                 * <FSK_Rb125Fd125>   highest FSK data rate (125kbs)
-                 * <FSK_Rb2Fd5>       lowe FSK data rate (2kbs)
-                 * <GFSK_Rb125Fd125>  highest GFSK rate GFSK (125kbs)
-                 * <GFSK_Rb2Fd5>      lowest GFSK data rate (2kbs)
-                 * <FSK_Rb_512Fd2_5>  original FSK test modulation (0.512kbs)
+                /* RFM23 modulation     schemes and data rates
+                 * <FSK_Rb125Fd125>     highest FSK data rate (125kbs)
+                 * <FSK_Rb2Fd5>         lowe FSK data rate (2kbs)
+                 * <GFSK_Rb125Fd125>    highest GFSK rate GFSK (125kbs)
+                 * <GFSK_Rb2Fd5>        lowest GFSK data rate (2kbs)
+                 * <FSK_Rb_512Fd2_5>    original FSK test modulation (0.512kbs)
                  */
 
                 rfm23.sleep();
@@ -61,29 +61,32 @@ namespace Artemis
                 rfm23.reset();
             }
 
-            void RFM23::send(const uint8_t *msg, size_t length)
+            void RFM23::send(PacketComm &packet)
             {
                 digitalWrite(RFM23_RX_ON, LOW);
                 digitalWrite(RFM23_TX_ON, LOW);
 
+                packet.wrapped.resize(0);
+                packet.Wrap();
+
                 Threads::Scope lock(spi1_mtx);
                 rfm23.setModeTx();
-                Serial.print(length);
-                rfm23.send(msg, length);
+                rfm23.send(packet.wrapped.data(), packet.wrapped.size());
                 rfm23.waitPacketSent();
 
                 rfm23.sleep();
                 rfm23.setModeIdle();
                 Serial.print("[RFM23] SENDING: [");
-                for (size_t i = 0; i < length; i++)
+                for (size_t i = 0; i < packet.wrapped.size(); i++)
                 {
-                    Serial.print(msg[i], HEX);
+                    Serial.print(packet.wrapped[i], HEX);
                 }
                 Serial.println("]");
             }
 
             bool RFM23::recv(PacketComm &packet)
             {
+                int32_t iretn = 0;
 
                 digitalWrite(RFM23_RX_ON, LOW);
                 digitalWrite(RFM23_TX_ON, HIGH);
@@ -100,8 +103,11 @@ namespace Artemis
                     if (rfm23.recv(packet.wrapped.data(), &bytes_recieved))
                     {
                         packet.wrapped.resize(bytes_recieved);
-                        packet.Unwrap();
+                        iretn = packet.Unwrap();
                         rfm23.setModeIdle();
+
+                        if (iretn < 0)
+                            return false;
 
                         return true;
                     }
