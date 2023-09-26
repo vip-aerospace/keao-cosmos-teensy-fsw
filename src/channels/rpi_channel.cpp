889b5cf49343ab31e3ab766d1d729ad176394685
montoyaoa <montoyao@hawaii.edu>
2023-09-26 13:21:49 -1000

    Format rpi_channel.cpp

--- src/channels/rpi_channel.cpp
+++ src/channels/rpi_channel.cpp
@@ -3,59 +3,53 @@
 
 void sendToPi();
 
-namespace
-{
-    using namespace Artemis;
-    PacketComm packet;
+namespace {
+  using namespace Artemis;
+  PacketComm packet;
+} // namespace
+
+void Artemis::Teensy::Channels::rpi_channel() {
+  Serial.println("RPI Thread starting..");
+  Serial2.begin(9600);
+
+  while (true) {
+    if (PullQueue(packet, rpi_queue, rpi_queue_mtx)) {
+      Serial.println((uint16_t)packet.header.type);
+      switch (packet.header.type) {
+        case PacketComm::TypeId::CommandEpsSwitchName:
+          if ((Artemis::Teensy::PDU::PDU_SW)packet.data[0] ==
+                  Artemis::Teensy::PDU::PDU_SW::RPI &&
+              packet.data[1] == 0) {
+            packet.header.type = PacketComm::TypeId::CommandObcHalt;
+            sendToPi();
+            threads.delay(20000); // Wait 20s to give the rpi time to turn off
+            digitalWrite(RPI_ENABLE, LOW);
+
+            // Empty RPI Queue
+            while (!rpi_queue.empty())
+              rpi_queue.pop_front();
+
+            Serial.println("Killing RPi thread");
+            kill_thread(Artemis::Teensy::Channels::Channel_ID::RPI_CHANNEL);
+            return;
+          }
+          break;
+        default:
+          sendToPi();
+          break;
+      }
+    }
+    threads.delay(100);
+  }
 }
 
-void Artemis::Teensy::Channels::rpi_channel()
-{
-    Serial.println("RPI Thread starting..");
-    Serial2.begin(9600);
-
-    while (true)
-    {
-        if (PullQueue(packet, rpi_queue, rpi_queue_mtx))
-        {
-            Serial.println((uint16_t)packet.header.type);
-            switch (packet.header.type)
-            {
-            case PacketComm::TypeId::CommandEpsSwitchName:
-                if ((Artemis::Teensy::PDU::PDU_SW)packet.data[0] == Artemis::Teensy::PDU::PDU_SW::RPI && packet.data[1] == 0)
-                {
-                    packet.header.type = PacketComm::TypeId::CommandObcHalt;
-                    sendToPi();
-                    threads.delay(20000); // Wait 20s to give the rpi time to turn off
-                    digitalWrite(RPI_ENABLE, LOW);
-
-                    // Empty RPI Queue
-                    while (!rpi_queue.empty())
-                        rpi_queue.pop_front();
-
-                    Serial.println("Killing RPi thread");
-                    kill_thread(Artemis::Teensy::Channels::Channel_ID::RPI_CHANNEL);
-                    return;
-                }
-                break;
-            default:
-                sendToPi();
-                break;
-            }
-        }
-        threads.delay(100);
-    }
-}
-
-void sendToPi()
-{
-    packet.SLIPPacketize();
-    Serial.println("Forwarding to RPi");
-    for (size_t i = 0; i < packet.packetized.size(); i++)
-    {
-        Serial.print((unsigned)packet.packetized[i]);
-        Serial.print(" ");
-        Serial2.write(packet.packetized[i]);
-    }
-    Serial.println();
+void sendToPi() {
+  packet.SLIPPacketize();
+  Serial.println("Forwarding to RPi");
+  for (size_t i = 0; i < packet.packetized.size(); i++) {
+    Serial.print((unsigned)packet.packetized[i]);
+    Serial.print(" ");
+    Serial2.write(packet.packetized[i]);
+  }
+  Serial.println();
 }
