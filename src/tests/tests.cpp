45909136c91017c00bc5f4c3bc30585820b098aa
montoyaoa <montoyao@hawaii.edu>
2023-09-28 13:59:39 -1000

    Make tests run conditionally based on build flags
    Tests will only run if the TESTS flag is set, and tests will only run
    for the devices that are enabled via the ENABLE_ flags.

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -10,9 +10,10 @@ namespace {
 } // namespace
 
 void run_test() {
+#ifdef TESTS
   Cosmos::Support::PacketComm packet;
 
-#ifdef TEST_PI
+#ifdef ENABLE_RASPBERRYPI
   packet.header.type     = PacketComm::TypeId::CommandEpsSwitchName;
   packet.header.nodeorig = (uint8_t)NODES::GROUND_NODE_ID;
   packet.header.nodedest = (uint8_t)NODES::TEENSY_NODE_ID;
@@ -22,7 +23,7 @@ void run_test() {
   PushQueue(packet, main_queue, main_queue_mtx);
 #endif
 
-#ifdef TEST_I2C_PI
+#ifdef ENABLE_RASPBERRYPI
   packet.header.type     = (PacketComm::TypeId)0x800;
   packet.header.nodeorig = (uint8_t)NODES::TEENSY_NODE_ID;
   packet.header.nodedest = (uint8_t)NODES::RPI_NODE_ID;
@@ -30,7 +31,7 @@ void run_test() {
   PushQueue(packet, rpi_queue, rpi_queue_mtx);
 #endif
 
-#ifdef TEST_PDU
+#ifdef ENABLE_PDU
   packet.header.type     = PacketComm::TypeId::CommandEpsSwitchName;
   packet.header.nodeorig = (uint8_t)NODES::GROUND_NODE_ID;
   packet.header.nodedest = (uint8_t)NODES::TEENSY_NODE_ID;
@@ -40,7 +41,7 @@ void run_test() {
   PushQueue(packet, main_queue, main_queue_mtx);
 #endif
 
-#ifdef TEST_CAMERACAPTURE
+#ifdef ENABLE_RASPBERRYPI
   packet.header.type     = PacketComm::TypeId::CommandCameraCapture;
   packet.header.nodeorig = (uint8_t)NODES::GROUND_NODE_ID;
   packet.header.nodedest = (uint8_t)NODES::RPI_NODE_ID;
@@ -48,7 +49,7 @@ void run_test() {
   PushQueue(packet, main_queue, main_queue_mtx);
 #endif
 
-#ifdef TEST_RPI_SHUTDOWN
+#ifdef ENABLE_RASPBERRYPI
   if (piShutdownTimer > 10000 && !isoff) {
     packet.header.type     = PacketComm::TypeId::CommandEpsSwitchName;
     packet.header.nodeorig = (uint8_t)NODES::GROUND_NODE_ID;
@@ -60,7 +61,7 @@ void run_test() {
   }
 #endif
 
-#ifdef TEST_RFM23
+#ifdef ENABLE_RFM23
   packet.header.type     = PacketComm::TypeId::DataObcResponse;
   packet.header.nodeorig = (uint8_t)NODES::TEENSY_NODE_ID;
   packet.header.nodedest = (uint8_t)NODES::GROUND_NODE_ID;
@@ -79,28 +80,42 @@ void run_test() {
   PushQueue(packet, main_queue, main_queue_mtx);
 #endif
 
-#ifdef TEST_THREADS
   for (auto &t : thread_list) {
     Serial.print("Thread ");
     Serial.print(t.thread_id);
     Serial.print(": ");
     Serial.println(threads.getState(t.thread_id));
   }
+
+#ifdef ENABLE_TEMPERATURESENSORS
+  devices.read_temperature(uptime);
 #endif
 
-#ifdef TEST_BEACONS
-  devices.read_temperature(uptime);
+#ifdef ENABLE_CURRENTSENSORS
   devices.read_current(uptime);
-  devices.read_imu(uptime);
-  devices.read_mag(uptime);
-  devices.read_gps(uptime);
+#endif
 
-  // PDU Switch Status
+#ifdef ENABLE_IMU
+  devices.read_imu(uptime);
+#endif
+
+#ifdef ENABLE_MAGNETOMETER
+  devices.read_mag(uptime);
+#endif
+
+#ifdef ENABLE_GPS
+  devices.read_gps(uptime);
+#endif
+
+// PDU Switch Status
+#ifdef ENABLE_PDU
   packet.header.type     = PacketComm::TypeId::CommandEpsSwitchStatus;
   packet.header.nodeorig = (uint8_t)NODES::GROUND_NODE_ID;
   packet.header.nodedest = (uint8_t)NODES::TEENSY_NODE_ID;
   packet.data.clear();
   packet.data.push_back((uint8_t)Artemis::Teensy::PDU::PDU_SW::All);
   PushQueue(packet, pdu_queue, pdu_queue_mtx);
+#endif
+
 #endif
 }
