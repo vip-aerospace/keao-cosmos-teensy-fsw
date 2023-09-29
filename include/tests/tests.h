49dcc390512edddee3863f2b59556ebc587d39a9
montoyaoa <99383055+montoyaoa@users.noreply.github.com>
2023-09-28 21:14:40 -1000

    Remove unused macros

--- include/tests/tests.h
+++ include/tests/tests.h
@@ -3,16 +3,6 @@
 #ifndef _TESTS_H
 #define _TESTS_H
 
-// #define TEST_PI
-// #define TEST_I2C_PI
-// #define TEST_ASTRODEV
-// #define TEST_PDU
-// #define TEST_CAMERACAPTURE
-// #define TEST_RPI_SHUTDOWN
-// #define TEST_RFM23
-// #define TEST_THREADS
-// #define TEST_BEACONS
-
 void run_test();
 
 #endif //_TESTS_H
